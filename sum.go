c488c61fed3b483ca240ed2269f6138d04d81201
Felipe Natan <felipenatan@novaandradina.org>
2022-05-16 22:38:34 -0400

    initial commit

---
+++ sum.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mult(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
+
+func SumX(a int, b int) int {
+	return a + b + a
+}
+
+func SumY(a int, b int) int {
+	return a + b + a
+}
+
+func SumZ(a int, b int) int {
+	return a + b + a
+}
